57b79ee2b9ab962e46ad3f52d96bc0a3cc27a17e
Bioin4matics <i.malashchuk@gmail.com>
2022-05-24 15:53:36 +0100

    added dir script

---
+++ make_dir.py
@@ -0,0 +1,15 @@
+import os
+
+cur_path = os.getcwd()
+
+os.mkdir(cur_path+"/SNP")
+os.mkdir(cur_path+"/SNP/archive")
+
+os.mkdir(cur_path+"/TSO500")
+os.mkdir(cur_path+"/TSO500/archive")
+
+os.mkdir(cur_path+"/WES")
+os.mkdir(cur_path+"/WES/archive")
+
+os.mkdir(cur_path+"/MokaPipe")
+os.mkdir(cur_path+"/MokaPipe/archive")
